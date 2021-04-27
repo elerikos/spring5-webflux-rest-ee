5aafd02db455674b0941e66d7f5703154e949aad
Errikos.Elpiziotis <elerikos@gmail.com>
2021-04-27 11:48:31 +0300

    First commit

---
+++ src/main/java/erik/springframework/spring5webfluxrestee/Spring5WebfluxRestEeApplication.java
@@ -0,0 +1,13 @@
+package erik.springframework.spring5webfluxrestee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestEeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebfluxRestEeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erik/springframework/spring5webfluxrestee/Spring5WebfluxRestEeApplicationTests.java
@@ -0,0 +1,13 @@
+package erik.springframework.spring5webfluxrestee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestEeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
